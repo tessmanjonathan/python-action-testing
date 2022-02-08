b7eb1d5047154bafbaa800bffe825f3f462a08b9
Jonathan Tessman <44707643+tessmanjonathan@users.noreply.github.com>
2022-02-07 18:49:47 -0600

    Update gitpush.sh

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -7,11 +7,11 @@ cd .;
 
 if [[ -n $(git status -s) ]]; then
     git fetch --depth=1;
-    git checkout $GITHUB_HEAD_REF;
+    git checkout $BRANCH_NAME;
     git add .;
     git -c user.name="GitHub Actions" -c user.email="actions@github.com" \
       commit -m "Auto commit from action" \
-      --author="$GITHUB_ACTOR";
+      --author="$ACTOR_NAME";
     git push origin;
 else
     echo "Working tree clean. Nothing to commit.";
